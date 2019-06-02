daa83d3bc8d4bf421fe4dc2fb02b2cc175d835f7
WzZzW <dinhhongoc@gmail.com>
2019-06-02 21:45:14 +0700

    Insert-Layouts-&-Fix-404-Error

---
+++ news/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs',
+  layoutsDir: 'views/_layouts'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.use('/admin/data', require('./routes/admin/data.route'))
+
+
+
+
+
+
+
+// Handle 404 error
+app.use((req, res) => {
+  res.status(400);
+  res.render('_error/404_error', {title: '404: File not found', error: 'File not found'});
+});
+
+app.listen(3000, () => {
+  console.log('Server is running at https://localhost:3000');
+});
