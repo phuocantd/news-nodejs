1bbaae5bb094b146021772d8fa1fc7c1d45eba29
WzZzW <dinhhongoc@gmail.com>
2019-06-11 11:03:31 +0700

    Fix post info

--- news/models/post.model.js
+++ news/models/post.model.js
@@ -5,6 +5,12 @@ module.exports = {
         return db.loadContent(`SELECT Content FROM DemoDB.Posts`);
     },
     content: id => {
-        return db.loadContent(`SELECT Content FROM DemoDB.Posts WHERE PostId = ${id}`);
+        return db.loadContent(`SELECT puuu.*, ptt.TagName
+        FROM (select p.*, puu.Name from DemoDB.Posts p inner join 
+        (select pu.PostId, u.* from DemoDB.Posts_Users pu inner join DemoDB.Users u on pu.Author = u.UserId) puu 
+        on p.PostId = puu.PostId) puuu inner join
+        (select pt.PostId, t.* from DemoDB.Posts_Tags pt inner join DemoDB.Tags t on pt.TagId = t.TagId) ptt
+        on puuu.PostId = ptt.PostId
+        WHERE puuu.PostId = ${id}`);
     },
 };
--- news/routes/post.route.js
+++ news/routes/post.route.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var post = require('../models/post.model');
+var moment = require('moment');
 
 var router = express.Router();
 
@@ -7,9 +8,12 @@ router.get('/:id', (req, res) => {
     var id = req.params.id;
     var content = post.content(id);
     content.then(rows => {
-        // console.log(rows);
+        // console.log(rows[0].Title);
+        // console.log(moment(rows[0].PublishedDate).add(1, 'day').format('LLL'));
         res.render('vwPost/index', {
-            content: rows
+            singleContent: (rows[0]),
+            multipleContent: rows,
+            time: moment(rows[0].PublishedDate).add(1, 'day').format('LLL')
         });
     }).catch(error => {
         console.log(error);
