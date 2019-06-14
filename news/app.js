c83ceca256ee4435820df2f29ed054ed65587c52
WzZzW <dinhhongoc@gmail.com>
2019-06-14 09:49:22 +0700

    Test gitignore

--- news/app.js
+++ news/app.js
@@ -32,5 +32,5 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000/');
+  console.log('Server is running at localhost:3000');
 });
