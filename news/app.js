dc427e1ff0d6630ef81cdf22fc0b534c27944a56
WzZzW <dinhhongoc@gmail.com>
2019-06-16 09:44:00 +0700

    Fix-homepage

--- news/app.js
+++ news/app.js
@@ -3,7 +3,8 @@ var exphbs = require('express-handlebars');
 var hbs_sections = require('express-handlebars-sections')
 var post = require('./models/post.model');
 var hbs_sections = require('express-handlebars-sections');
-var morgan=require('morgan');
+var morgan = require('morgan');
+var moment = require('moment');
 
 var app = express();
 
@@ -25,30 +26,50 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   // res.render('home');
-  var query1 = post.content();
-  query1.then(rows => {
-    var query2 = post.content;
-
-
-
-
-
-
-
-
-
-    // if (rows.length > 0) {
-    //   res.render('home', {
-    //     singleContent: (rows[0]),
-    //     multipleContent: rows,
-    //     time: moment(rows[0].PublishedDate).add(1, 'day').format('LLL')
-    //   });
-    // }
-    // else {
-    //   res.render('_error/404_error', { title: '404: File not found', error: 'File not found', layout: false });
-    // }
+  var query1 = post.lastestNews();
+  query1.then(rows1 => {
+    var query2 = post.newsPost();
+    query2.then(rows2 => {
+      var query3 = post.phonePost();
+      query3.then(rows3 => {
+        var query4 = post.laptopPost();
+        query4.then(rows4 => {
+          var query5 = post.techPost();
+          query5.then(rows5 => {
+            if (rows1.length > 0) {
+              res.render('home', {
+                content: (rows1),
+                newsContent: (rows2),
+                phoneContent: (rows3),
+                laptopContent: (rows4),
+                techContent: (rows5),
+                content1: (rows1[0]),
+                time1: moment(rows1[0].PublishedDate).add(1, 'day').format('LLL'),
+                content2: (rows1[0]),
+                time2: moment(rows1[0].PublishedDate).add(1, 'day').format('LLL'),
+                content3: (rows1[1]),
+                time3: moment(rows1[1].PublishedDate).add(1, 'day').format('LLL'),
+                content4: (rows1[1]),
+                time4: moment(rows1[1].PublishedDate).add(1, 'day').format('LLL'),
+                content5: (rows1[1]),
+                time5: moment(rows1[1].PublishedDate).add(1, 'day').format('LLL'),
+              });
+            }
+          }).catch(error => {
+            console.log(error);
+          });
+        }).catch(error => {
+          console.log(error);
+        });
+      }).catch(error => {
+        console.log(error);
+      });
+    }).catch(error => {
+      console.log(error);
+    });
   }).catch(error => {
     console.log(error);
+    res.render('_error/404_error', { title: '404: File not found', error: 'File not found', layout: false });
   });
 });
 
--- news/utils/db.js
+++ news/utils/db.js
@@ -4,15 +4,13 @@ var createConnection = () => {
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '12345',
+        password: 'Tintin579314',
         database: 'DemoDB'
     });
 };
 
-
-
 module.exports = {
-    loadPostContent: sql => {
+    loadPost: sql => {
         return new Promise((resolve, reject) => {
             var connection = createConnection();
             connection.connect();
